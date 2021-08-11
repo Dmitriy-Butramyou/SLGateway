a8a46da2e4d41501cd5888b017cca82fe92abbad
D_Butramyou <d.butramyou@gmail.com>
2021-08-11 11:45:59 +0300

    Initial commit

---
+++ src/main/java/com/butramyou/slgateway/SlGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.butramyou.slgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/butramyou/slgateway/SlGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.butramyou.slgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
